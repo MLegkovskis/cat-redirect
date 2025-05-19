594238f09acba8678cebc8b9d3130b9b94e5a3a0
Mark Legkovskis <55879905+MLegkovskis@users.noreply.github.com>
2025-05-19 20:05:42 +0000

    first commit

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.title("🚚 UncertaintyCat has moved!")
+
+st.markdown(
+    """
+    The app now lives at &nbsp;[**uncertaintycat.streamlit.app**](https://uncertaintycat.streamlit.app)  
+    Click the link above (or the big cat emoji 😊) to open it in your browser!
+    """,
+    unsafe_allow_html=True,
+)
